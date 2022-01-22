d01965db48b9de51d7804692175ccbbca71e6588
Szymon Szostak <21984800+xszym@users.noreply.github.com>
2022-01-22 16:50:06 +0100

    Init tournament models

--- tournamentmanager/tournament/admin.py
+++ tournamentmanager/tournament/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.admin import site
+import adminactions.actions as actions
 
-# Register your models here.
+from .models import Game, Tournament, Team, Score, Match
+actions.add_to_site(site)
+
+admin.site.register(Game)
+admin.site.register(Tournament)
+admin.site.register(Team)
+admin.site.register(Score)
+admin.site.register(Match)
