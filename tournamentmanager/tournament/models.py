22b42c953647e48a1169430517996e35cafbff84
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-01-24 16:18:44 +0100

    Remove unnecessary print statement

--- tournamentmanager/tournament/models.py
+++ tournamentmanager/tournament/models.py
@@ -28,7 +28,6 @@ class EliminationType(Enum):
 
     @classmethod
     def choices(cls):
-        print(tuple((i.name, i.value) for i in cls))
         return tuple((i.name, i.value) for i in cls)
 
 
