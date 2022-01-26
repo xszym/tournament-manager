ad9305a66b093a944fb3e4e5c6f4e8395f765de7
Mateusz Piotr Moruś <mpmorus@gmail.com>
2022-01-26 19:40:22 +0100

    Make models' primary keys a UUID fields

--- tournamentmanager/tournamentmanager/settings.py
+++ tournamentmanager/tournamentmanager/settings.py
@@ -19,11 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', 1)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 1)
+if not SECRET_KEY and DEBUG:
+    # Set up debug secret key if debug environment is active
+    SECRET_KEY = 'not-so-secret-in-debug-mode'
+
 
 ALLOWED_HOSTS = ['*']
 
